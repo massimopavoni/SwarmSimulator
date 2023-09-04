a700eb089c549c0aa9e90916cb112d5a378c2b26
Massimo Pavoni <maspavoni@gmail.com>
2023-09-04 18:50:31 +0200

    Small chart controller continuation + graphics
    - chart controller main methods are still in development, will tackle the actual drawing on it, finally; wish me luck
    - added many images, grouped svgs in inner directory, updated simulator fxml
    - might or might not do some more graphics designing before tackling swarm drawing

--- simulator/src/main/java/it/unicam/cs/massimopavoni/swarmsimulator/simulator/view/gui/SwarmChartController.java
+++ simulator/src/main/java/it/unicam/cs/massimopavoni/swarmsimulator/simulator/view/gui/SwarmChartController.java
@@ -7,13 +7,15 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 import java.util.List;
-import java.util.function.DoubleToLongFunction;
+import java.util.function.DoubleFunction;
 
 public final class SwarmChartController {
-    public static final double DEFAULT_X_AXIS_SPAN = 14;
-    public static final double DEFAULT_TICK_UNIT = 10;
     public static final Position DEFAULT_ORIGIN = new Position(0, 0);
-    private static final DoubleToLongFunction DEFAULT_AXIS_ROUNDING = Math::round;
+    public static final double DEFAULT_X_AXIS_SPAN = 14;
+    public static final double SMALLEST_X_AXIS_SPAN = Math.pow(2, -8) * DEFAULT_X_AXIS_SPAN;
+    public static final double BIGGEST_X_AXIS_SPAN = Double.MAX_VALUE;
+    public static final double Y_AXIS_TICK_COUNT = 10;
+    private static final DoubleFunction<Double> AXIS_SPAN_TRANSFORM = Math::abs;
     private final XYChart<Number, Number> chart;
     private final NumberAxis xAxis;
     private final NumberAxis yAxis;
@@ -33,6 +35,10 @@ public final class SwarmChartController {
     public void setSources(List<Region> domain, List<Drone> swarm) {
         this.domain = domain;
         this.swarm = swarm;
+    }
+
+    public void resetPosition() {
+        translateAxes(DEFAULT_ORIGIN);
     }
 
     public void translateAxes(Position destination) {
@@ -56,20 +62,32 @@ public final class SwarmChartController {
     }
 
     private void translateAxis(NumberAxis axis, double delta) {
+        if (axis.getLowerBound() + delta < -BIGGEST_X_AXIS_SPAN)
+            delta = -BIGGEST_X_AXIS_SPAN - axis.getLowerBound();
+        else if (axis.getUpperBound() + delta > BIGGEST_X_AXIS_SPAN)
+            delta = BIGGEST_X_AXIS_SPAN - axis.getUpperBound();
         axis.setLowerBound(axis.getLowerBound() + delta);
         axis.setUpperBound(axis.getUpperBound() + delta);
     }
 
-    public void zoom(double zoomFactor) {
+    public void resetZoom() {
+        scaleAxes(DEFAULT_X_AXIS_SPAN);
+    }
 
+    public void zoom(double zoomFactor) {
+        scaleAxes((xAxis.getUpperBound() - xAxis.getLowerBound()) * zoomFactor);
     }
 
     private void scaleAxes(double newXAxisSpan) {
-        double newYAxisSpan = DEFAULT_AXIS_ROUNDING.applyAsLong(newXAxisSpan / xyAxesRatio);
-        newXAxisSpan = DEFAULT_AXIS_ROUNDING.applyAsLong(newYAxisSpan * xyAxesRatio);
+        if (newXAxisSpan < SMALLEST_X_AXIS_SPAN)
+            newXAxisSpan = SMALLEST_X_AXIS_SPAN;
+        else if (newXAxisSpan > BIGGEST_X_AXIS_SPAN)
+            newXAxisSpan = BIGGEST_X_AXIS_SPAN;
+        double newYAxisSpan = AXIS_SPAN_TRANSFORM.apply(newXAxisSpan / xyAxesRatio);
+        newXAxisSpan = newYAxisSpan * xyAxesRatio;
         scaleAxis(xAxis, newXAxisSpan);
         scaleAxis(yAxis, newYAxisSpan);
-        double newTickUnit = newYAxisSpan / DEFAULT_TICK_UNIT;
+        double newTickUnit = newYAxisSpan / Y_AXIS_TICK_COUNT;
         xAxis.setTickUnit(newTickUnit);
         yAxis.setTickUnit(newTickUnit);
     }
@@ -80,13 +98,13 @@ public final class SwarmChartController {
         axis.setUpperBound(axis.getUpperBound() + spanIncreaseHalf);
     }
 
-    private void update() {
-        chart.getData().clear();
-    }
-
     public void reset() {
         chart.getData().clear();
-        translateAxes(DEFAULT_ORIGIN);
-        scaleAxes(DEFAULT_X_AXIS_SPAN);
+        resetPosition();
+        resetZoom();
+    }
+
+    public void update() {
+        chart.getData().clear();
     }
 }
