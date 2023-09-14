571de20b20f54f0775f75de16905a17b794b0c20
Massimo Pavoni <maspavoni@gmail.com>
2023-09-14 19:15:46 +0200

    Updating exports mainly for javadoc for application module

--- simulator/src/main/java/module-info.java
+++ simulator/src/main/java/module-info.java
@@ -10,4 +10,10 @@ module it.unicam.cs.massimopavoni.swarmsimulator.simulator {
     opens it.unicam.cs.massimopavoni.swarmsimulator.simulator.view.gui to javafx.graphics, javafx.fxml;
     opens it.unicam.cs.massimopavoni.swarmsimulator.simulator.view.gui.control to javafx.controls, javafx.fxml;
     opens it.unicam.cs.massimopavoni.swarmsimulator.simulator to javafx.graphics, javafx.fxml;
+
+    exports it.unicam.cs.massimopavoni.swarmsimulator.simulator;
+    exports it.unicam.cs.massimopavoni.swarmsimulator.simulator.launcher;
+    exports it.unicam.cs.massimopavoni.swarmsimulator.simulator.view;
+    exports it.unicam.cs.massimopavoni.swarmsimulator.simulator.view.gui;
+    exports it.unicam.cs.massimopavoni.swarmsimulator.simulator.view.gui.control;
 }
